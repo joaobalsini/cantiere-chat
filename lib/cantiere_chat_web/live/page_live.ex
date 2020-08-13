7a73cce9e93610ef571ffac56fb17f931c00806f
João Gilberto Balsini Moura <joaobalsini@Joaos-MacBook-Pro-2.local>
2020-08-13 20:33:46 -0300

    Add live chat for single user

--- lib/cantiere_chat_web/live/page_live.ex
+++ lib/cantiere_chat_web/live/page_live.ex
@@ -3,11 +3,56 @@ defmodule CantiereChatWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, user_name: "")}
+    {:ok, socket}
   end
 
   @impl true
   def handle_event("join", %{"user_name" => user_name}, socket) do
-    {:noreply, assign(socket, user_name: user_name )}
+    {:noreply,
+     socket
+     |> push_patch(to: Routes.page_path(socket, :chat, user_name: user_name))}
+  end
+
+  @impl true
+  def handle_event("send", %{"message" => ""}, socket), do: {:noreply, socket}
+
+  @impl true
+  def handle_event("send", %{"message" => message}, socket) do
+    user_name = socket.assigns.user_name
+    new_message = %{user: user_name, message: message}
+
+    messages = List.insert_at(socket.assigns.messages, -1, new_message)
+
+    {:noreply,
+     socket
+     |> assign(message: "", messages: messages)}
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    live_action = socket.assigns.live_action
+    {:noreply, apply_action(socket, live_action, params)}
+  end
+
+  def apply_action(socket, :chat, %{"user_name" => ""}), do: redirect_back(socket)
+
+  def apply_action(socket, :chat, %{"user_name" => user_name}) do
+    socket
+    |> assign(user_name: user_name)
+    |> assign(messages: [])
+    |> assign(message: "")
+  end
+
+  def apply_action(socket, :chat, _other), do: redirect_back(socket)
+
+  def apply_action(socket, :index, _params) do
+    socket
+    |> assign(user_name: "")
+  end
+
+  defp redirect_back(socket) do
+    socket
+    |> put_flash(:error, "You need to set your name to enter the chat.")
+    |> push_redirect(to: Routes.page_path(socket, :index))
   end
 end
--- lib/cantiere_chat_web/router.ex
+++ lib/cantiere_chat_web/router.ex
@@ -18,6 +18,7 @@ defmodule CantiereChatWeb.Router do
     pipe_through :browser
 
     live "/", PageLive, :index
+    live "/chat", PageLive, :chat
   end
 
   # Other scopes may use custom stacks.
